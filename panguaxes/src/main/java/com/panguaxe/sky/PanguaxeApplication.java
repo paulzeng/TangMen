f826169a1b122716b458a7788abaa4bbe54b5645
Panguaxe <panguaxes@aliyun.com>
2019-07-16 22:14:51 +0800

    无修改  git提交测试

--- panguaxes/src/main/java/com/panguaxe/sky/PanguaxeApplication.java
+++ panguaxes/src/main/java/com/panguaxe/sky/PanguaxeApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
-
 /**
  * @Author 作者：Panguaxe
  * @Desc	TODO
